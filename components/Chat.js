c2d04207dc8b9432f732ce57f8fe587977ac21ac
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-06-30 14:36:16 -0600

    5.3

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,70 @@
 import React, { Component } from 'react';
-import { StyleSheet, ImageBackground, Text, TextInput, Alert, TouchableOpacity, Button, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+import { StyleSheet, ImageBackground, Text, TextInput, Alert, TouchableOpacity, Button, View, Platform } from 'react-native';
+
+class Example extends React.Component {
+    state = {
+        messages: []
+    };
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: 'This is a system message',
+                    createdAt: new Date(),
+                    system: true,
+                },
+            ]
+        })
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }))
+    }
+
+    renderBubble(props) {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#000'
+                    }
+                }}
+            />
+        )
+    }
+
+    render() {
+        return (
+            <View>
+                <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{
+                        _id: 1,
+                    }}
+                />
+                {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
+            </View>
+        )
+    }
+}
 
 export default class Chat extends React.Component {
     static navigationOptions = ({ navigation }) => {
