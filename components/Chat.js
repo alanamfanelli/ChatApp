7c1d237733e560c0921295f55fdc9730eb049301
alanamfanelli <56361850+alanamfanelli@users.noreply.github.com>
2020-07-20 20:25:54 -0600

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -77,13 +77,13 @@ export default class Chat extends React.Component {
                 this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
                     if (!user) {
                         firebase.auth().signInAnonymously();
+                    } else {
+
+                        this.setState({
+                            uid: user.uid,
+                            messages: []
+                        });
                     }
-
-                    this.setState({
-                        uid: user.uid,
-                        messages: []
-                    });
-
                     this.unsubscribe = this.referenceChatMessages.orderBy('createdAt', 'desc').onSnapshot(this.onCollectionUpdate);
                 });
             } else {
